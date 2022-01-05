b7ca02b7aa030331b60cb3ddc0233b4ed83ed3ca
Grant Spraker <grant.spraker@gmail.com>
2022-01-05 11:58:51 -0700

    got to slide 5

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController,UITableViewDataSource
+{
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 3
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Celled", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
 
